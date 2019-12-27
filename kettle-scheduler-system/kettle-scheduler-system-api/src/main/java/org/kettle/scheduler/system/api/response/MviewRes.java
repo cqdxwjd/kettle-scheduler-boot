7886da4c9b3d7f3299f16a483b0d644e44eaa5d0
leo <chen.1076@qq.com>
2019-12-27 11:56:04 +0800

    1、物化视图出参增加对应字段

--- kettle-scheduler-system/kettle-scheduler-system-api/src/main/java/org/kettle/scheduler/system/api/response/MviewRes.java
+++ kettle-scheduler-system/kettle-scheduler-system-api/src/main/java/org/kettle/scheduler/system/api/response/MviewRes.java
@@ -1,8 +1,11 @@
 package org.kettle.scheduler.system.api.response;
 
-import org.kettle.scheduler.system.api.basic.BasicVO;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * 描述:
@@ -10,6 +13,32 @@ import java.io.Serializable;
  * @author leo
  * @create 2019-12-26 18:03
  */
-public class MviewRes extends BasicVO implements Serializable {
+@Data
+@ApiModel(value = "物化视图表-出参")
+public class MviewRes implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "所有者")
+    private String owner;
+
+    @ApiModelProperty(value = "视图名称")
+    private String mviewName;
+
+    @ApiModelProperty(value = "最后刷新时间")
+    private Date lastRefreshDate;
+
+    @ApiModelProperty(value = "刷新方法")
+    private String refreshMethod;
+
+    @ApiModelProperty(value = "是否失效")
+    private String invalid;
+
+    @ApiModelProperty(value = "物化视图标签ID")
+    private String mviewTagId;
+
+    @ApiModelProperty(value = "物化视图查询语句")
+    private String query;
+
+    @ApiModelProperty(value = "是否启用：1启用、0停用")
+    private String status;
 }
