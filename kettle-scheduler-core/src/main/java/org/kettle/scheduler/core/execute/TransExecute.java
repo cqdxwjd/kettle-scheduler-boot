d940947ed9341530e87169b188973bc43161410a
chenzhao <chen.1076@qq.com>
2020-06-16 09:58:53 +0800

    1、同一脚本，同时运行时，日志会串。原因：LogChannelID是根据transName和stepName来获取的，便于处理，在脚本和步骤名称中加入随机数，保证名称不冲突问题，计算出来的logChannelId不会出现重复的情况

--- kettle-scheduler-core/src/main/java/org/kettle/scheduler/core/execute/TransExecute.java
+++ kettle-scheduler-core/src/main/java/org/kettle/scheduler/core/execute/TransExecute.java
@@ -15,6 +15,7 @@ import org.pentaho.di.repository.AbstractRepository;
 import org.pentaho.di.repository.RepositoryDirectoryInterface;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
+import org.pentaho.di.trans.step.StepMeta;
 
 import java.util.*;
 
@@ -29,24 +30,29 @@ public class TransExecute {
     /**
      * ktr执行单元，所有的ktr执行都调用该方法
      *
-     * @param tm ktr元数据
+     * @param tm     ktr元数据
      * @param params ktr需要的命名参数
-     * @param args 命令行参数
+     * @param args   命令行参数
      */
-    private static String executeTrans(TransMeta tm, Map<String, String> params, String[] args,LogLevel logLevel) throws KettleException {
+    private static String executeTrans(TransMeta tm, Map<String, String> params, String[] args, LogLevel logLevel) throws KettleException {
         // 通过元数据获取ktr的实例
         Random random = new Random();
         int i = random.nextInt(1000);
+        //设置变动的stepName
+        List<StepMeta> steps = tm.getSteps();
+        steps.forEach(stepMeta -> {
+            stepMeta.setName(stepMeta.getName() + random.nextInt(1000));
+        });
         Trans trans = new Trans(tm);
-        LogChannel logChannel=new LogChannel(trans.getName()+i+System.currentTimeMillis());
+        LogChannel logChannel = new LogChannel(trans.getName() + i + System.currentTimeMillis());
         trans.setLog(logChannel);
-        if(logLevel!=null){
+        if (logLevel != null) {
             trans.setLogLevel(logLevel);
         }
         // 传入ktr需要的变量
         if (CollectionUtil.isNotEmpty(params)) {
-			log.info("传入kettle的参数：{}", JsonUtil.toJsonString(params));
-			params.forEach((String k,Object v)->{
+            log.info("传入kettle的参数：{}", JsonUtil.toJsonString(params));
+            params.forEach((String k, Object v) -> {
                 try {
                     trans.setParameterValue(k, v.toString());
                 } catch (UnknownParamException e) {
@@ -67,7 +73,6 @@ public class TransExecute {
         String logText = KettleLogUtil.getLogText(trans.getLogChannelId(), true, trans.getLogDate().getTime());
 
 
-
         // 判断执行过程中是否有错误, 有错误就抛出错误日志
         if (trans.getErrors() > 0) {
             throw new KettleException(logText);
@@ -80,25 +85,25 @@ public class TransExecute {
      * 运行单个ktr
      *
      * @param fullPathName ktr全路径名
-     * @param params ktr需要的命名参数
-     * @param logLevel 日志级别
+     * @param params       ktr需要的命名参数
+     * @param logLevel     日志级别
      */
     public static String run(String fullPathName, Map<String, String> params, LogLevel logLevel) throws KettleException {
         // 通过ktr全路径名获取ktr元数据
         TransMeta tm = new TransMeta(FileUtil.replaceSeparator(fullPathName));
         // 开始执行ktr
-        return executeTrans(tm, params, null,logLevel);
+        return executeTrans(tm, params, null, logLevel);
     }
 
     /**
      * 运行资源库中的ktr
      *
-     * @param rep 资源库对象
-     * @param transPath ktr所在路径
-     * @param transName ktr名称
+     * @param rep          资源库对象
+     * @param transPath    ktr所在路径
+     * @param transName    ktr名称
      * @param versionLabel 版本号，传入null表示执行最新的ktr
-     * @param params ktr需要的命名参数
-     * @param logLevel 日志级别
+     * @param params       ktr需要的命名参数
+     * @param logLevel     日志级别
      */
     public static String run(AbstractRepository rep, String transPath, String transName, String versionLabel, Map<String, String> params, LogLevel logLevel) throws KettleException {
         // 根据相对目录地址获取ktr所在目录信息
@@ -106,6 +111,6 @@ public class TransExecute {
         // 在指定资源库的目录下找到要执行的转换
         TransMeta tm = rep.loadTransformation(transName, rdi, new ProgressNullMonitorListener(), true, versionLabel);
         // 开始执行ktr
-        return executeTrans(tm, params, null,logLevel);
+        return executeTrans(tm, params, null, logLevel);
     }
 }
